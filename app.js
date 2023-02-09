49b2aa16f71675358a5ec04e6fa5e87a69ec4cb4
Wayne Sun <121570457+WeiWayne1030@users.noreply.github.com>
2023-02-09 11:52:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const filteredRestaurant = restaurants.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    return restaurant.name.toLowerCase().includes(keyword) ||
+    restaurant.category.includes(keyword)
   })
   res.render('index', { restaurants: filteredRestaurant, keyword: keyword })
 })
